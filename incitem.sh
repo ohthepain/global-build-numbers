d9c81e4d83cba33f59bfe7938229ac1e91fa4a4a
Paul Wilkinson <cremoni@gmail.com>
2024-05-10 06:37:14 +0200

    gitignore

--- incitem.sh
+++
@@ -1,5 +0,0 @@
-aws dynamodb update-item --table-name build-numbers --key '{"product-id":{"S":"My Product"}}' \
-    --update-expression "SET VERSION = VERSION + :incr" \
-    --expression-attribute-values '{":incr":{"N":"1"}}' \
-    --return-values UPDATED_NEW --region eu-central-1 --output text | awk '{print $2}'
-
--- putitem.sh
+++
@@ -1,1 +0,0 @@
-aws dynamodb put-item --table-name build-numbers --item '{"product-id":{"S":"My Product"},"VERSION":{"N":"1"}}'
