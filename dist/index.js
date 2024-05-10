872693619614c460d8ba6c5940bcbf0001c5e5be
Paul Wilkinson <cremoni@gmail.com>
2024-05-10 18:51:42 +0200

    forgot to build

--- dist/index.js
+++ dist/index.js
@@ -26147,6 +26147,178 @@ exports["default"] = _default;
 
 /***/ }),
 
+/***/ 2894:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const { exec } = __nccwpck_require__(3409)
+const core = __nccwpck_require__(9935);
+
+async function increment(dynamoTableName, dynamoPartitionKey, productId) {
+    try {
+        let stdout = '';
+        let stderr = '';
+
+        const options = {
+            listeners: {
+                stdout: (data) => {
+                    stdout += data.toString();
+                },
+                stderr: (data) => {
+                    stderr += data.toString();
+                }
+            }
+        };
+
+        const keyJson = { [dynamoPartitionKey]: { "S": `${productId}` } };        
+        const key = JSON.stringify(keyJson);
+        const tableName = `${dynamoTableName}`;
+        const incrExpression = {":incr":{"N":"1"}};
+        const incrJson = JSON.stringify(incrExpression);
+
+        await exec("aws", [
+            "dynamodb", 
+            "update-item", 
+            "--table-name",
+            tableName,
+            "--key",
+            key,
+            "--update-expression",
+            "SET VERSION = VERSION + :incr",
+            "--expression-attribute-values",
+            incrJson,
+            "--return-values",
+            "UPDATED_NEW",
+            "--output",
+            "text",
+        ], options);
+
+        // console.log(`stdOutResults ${stdout}, stdErrResults ${stderr}`)
+
+        const result = stdout.match(/\w+\s+(\d+)/)[1];
+        // console.log(`result ${result}`)
+        core.setOutput("result", result);
+    } catch (error) {
+        if (error instanceof Error) {
+            if (core.getInput('CREATE_PRODUCT_ID')) {
+                console.log(`Error: maybe productId=${productId} doesn't exist. Try setting it to 0`);
+                await set(dynamoTableName, dynamoPartitionKey, productId, 0);
+            } else {
+                console.log(`Error: maybe productId=${productId} doesn't exist. Exit.`);
+                core.setFailed(error.message); // Properly log the error message and fail the action
+            }
+        } else {
+            console.log('Failed to increment');
+        }
+    }
+}
+
+async function get(dynamoTableName, dynamoPartitionKey, productId) {
+    try {
+        const keyJson = { [dynamoPartitionKey]: { "S": `${productId}` } };        
+        const key = JSON.stringify(keyJson);
+
+        let value;
+        const command = `aws dynamodb get-item --table-name ${dynamoTableName} --key`
+        // console.log(command);
+        await exec(command, [key], {
+            listeners: {
+                stdout: (data) => {
+                    const output = data.toString();
+                    // console.log(output)
+                    value = JSON.parse(output).Item.VERSION.N;
+                    // console.log(value)
+                },
+                stderr: (data) => {
+                    console.error(data.toString());
+                }
+            }
+        });
+        core.setOutput("result", value);
+        return value;
+    } catch (error) {
+        console.error("Error getting value from DynamoDB item:", error);
+        return null;
+    }    
+}
+
+async function set(dynamoTableName, dynamoPartitionKey, productId, value) {
+    try {
+        let stdout = '';
+        let stderr = '';
+    
+        const options = {
+            listeners: {
+                stdout: (data) => {
+                    console.log(`stdout: <${data.toString()}>`)
+                    stdout += data.toString();
+                },
+                stderr: (data) => {
+                    console.log(`stderr: <${data.toString()}>`)
+                    stderr += data.toString();
+                }
+            }
+        };    
+
+        const item = {
+            [dynamoPartitionKey]: { "S": `${productId}` }, // Use productId as the value
+            "VERSION": { "N": `${value}` }
+        };
+        const itemJson = JSON.stringify(item);
+        // console.log(`itemJson ${itemJson}`);
+
+        const awsCommand = `aws dynamodb put-item --table-name ${dynamoTableName} --item `;
+        // console.log(`${awsCommand}`);
+        var returnValue = await exec(awsCommand, [itemJson], options);
+        // console.log(`returnValue ${returnValue} stdOutResults ${stdout}, stdErrResults ${stderr}`)
+
+        core.setOutput("result", value);
+    } catch (error) {
+        if (error instanceof Error) {
+            core.setFailed(error.message); // Properly log the error message and fail the action
+        } else {
+            console.log('Failed to set');
+        }
+    }
+}
+
+async function main() {
+    const dynamoTableName = core.getInput('DYNAMO_TABLE_NAME');
+    const dynamoPartitionKey = core.getInput('DYNAMO_PARTITION_KEY');
+    const productId = core.getInput('PRODUCT_ID');
+
+    const op = core.getInput('OPERATION');
+    switch (op) {
+        case "increment":
+            await increment(dynamoTableName, dynamoPartitionKey, productId);
+            break;
+        case "get":
+            await get(dynamoTableName, dynamoPartitionKey, productId);
+            break;
+        case "set":
+            const value = core.getInput('VALUE');
+            await set(dynamoTableName, dynamoPartitionKey, productId, value);
+            break;
+        default:
+            core.setFailed("OPERATION must be one of [set | increment]")
+    }
+}
+
+async function mainDebug() {
+    const dynamoTableName = "build-numbers"
+    const dynamoPartitionKey = "product-id"
+    const productId = "My New Product"
+    // const value = "77";
+
+    increment(dynamoTableName, dynamoPartitionKey, productId);
+}
+
+main();
+
+module.exports = { increment, get, set, main };
+
+
+/***/ }),
+
 /***/ 9491:
 /***/ ((module) => {
 
@@ -28045,224 +28217,17 @@ module.exports = parseParams
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__nccwpck_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__nccwpck_require__.o(definition, key) && !__nccwpck_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__nccwpck_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__nccwpck_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
 /******/ 	
 /************************************************************************/
-var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be in strict mode.
-(() => {
-"use strict";
-__nccwpck_require__.r(__webpack_exports__);
-/* harmony export */ __nccwpck_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
-/* harmony export */ });
-const { exec } = __nccwpck_require__(3409)
-const core = __nccwpck_require__(9935);
-
-async function increment() {
-    try {
-        // const dynamoTableName = core.getInput('DYNAMO_TABLE_NAME');
-        // const dynamoPartitionKey = core.getInput('DYNAMO_PARTITION_KEY');
-        // const productId = core.getInput('PRODUCT_ID');
-
-        const dynamoTableName = "build-numbers"
-        const dynamoPartitionKey = "product-id"
-        const productId = "Unknown product"
-
-        let stdout = '';
-        let stderr = '';
-
-        const options = {
-            listeners: {
-                stdout: (data) => {
-                    stdout += data.toString();
-                },
-                stderr: (data) => {
-                    stderr += data.toString();
-                }
-            }
-        };
-
-        const keyJson = { [dynamoPartitionKey]: { "S": `${productId}` } };        
-        const key = JSON.stringify(keyJson);
-        const tableName = `${dynamoTableName}`;
-        const incrExpression = {":incr":{"N":"1"}};
-        const incrJson = JSON.stringify(incrExpression);
-
-        await exec("aws", [
-            "dynamodb", 
-            "update-item", 
-            "--table-name",
-            tableName,
-            "--key",
-            key,
-            "--update-expression",
-            "SET VERSION = VERSION + :incr",
-            "--expression-attribute-values",
-            incrJson,
-            "--return-values",
-            "UPDATED_NEW",
-            "--output",
-            "text",
-        ], options);
-
-        // console.log(`stdOutResults ${stdout}, stdErrResults ${stderr}`)
-
-        const result = stdout.match(/\w+\s+(\d+)/)[1];
-        // console.log(`result ${result}`)
-        core.setOutput("result", result);
-    } catch (error) {
-        if (error instanceof Error) {
-            core.setFailed(error.message); // Properly log the error message and fail the action
-        } else {
-            console.log('Failed to increment');
-        }
-    }
-}
-
-async function get() {
-    try {
-        // const dynamoTableName = core.getInput('DYNAMO_TABLE_NAME');
-        // const dynamoPartitionKey = core.getInput('DYNAMO_PARTITION_KEY');
-        // const productId = core.getInput('PRODUCT_ID');
-
-        const dynamoTableName = "build-numbers"
-        const dynamoPartitionKey = "product-id"
-        const productId = "My Product"
-
-        const keyJson = { [dynamoPartitionKey]: { "S": `${productId}` } };        
-        const key = JSON.stringify(keyJson);
-
-        let value;
-        const command = `aws dynamodb get-item --table-name ${dynamoTableName} --key`
-        // console.log(command);
-        await exec(command, [key], {
-            listeners: {
-                stdout: (data) => {
-                    const output = data.toString();
-                    // console.log(output)
-                    value = JSON.parse(output).Item.VERSION.N;
-                    // console.log(value)
-                },
-                stderr: (data) => {
-                    console.error(data.toString());
-                }
-            }
-        });
-        core.setOutput("result", value);
-        return value;
-    } catch (error) {
-        console.error("Error getting value from DynamoDB item:", error);
-        return null;
-    }    
-}
-
-async function set() {
-    try {
-        // const awsAccessKeyId = core.getInput('AWS_ACCESS_KEY_ID');
-        // const awsSecretAccessKey = core.getInput('AWS_SECRET_ACCESS_KEY');
-        // const awsDefaultRegion = core.getInput('AWS_DEFAULT_REGION');
-
-        const dynamoTableName = core.getInput('DYNAMO_TABLE_NAME');
-        const dynamoPartitionKey = core.getInput('DYNAMO_PARTITION_KEY');
-        const productId = core.getInput('PRODUCT_ID');
-        const value = core.getInput('VALUE');
-        // const dynamoTableName = "build-numbers"
-        // const dynamoPartitionKey = "product-id"
-        // const productId = "My Product"
-        // const value = "77";
-
-        let stdout = '';
-        let stderr = '';
-    
-        const options = {
-            listeners: {
-                stdout: (data) => {
-                    console.log(`stdout: <${data.toString()}>`)
-                    stdout += data.toString();
-                },
-                stderr: (data) => {
-                    console.log(`stderr: <${data.toString()}>`)
-                    stderr += data.toString();
-                }
-            }
-        };    
-
-        const item = {
-            [dynamoPartitionKey]: { "S": `${productId}` }, // Use productId as the value
-            "VERSION": { "N": `${value}` }
-        };
-        const itemJson = JSON.stringify(item);
-        // console.log(`itemJson ${itemJson}`);
-
-        const awsCommand = `aws dynamodb put-item --table-name ${dynamoTableName} --item `;
-        // console.log(`${awsCommand}`);
-        var returnValue = await exec(awsCommand, [itemJson], options);
-        // console.log(`returnValue ${returnValue} stdOutResults ${stdout}, stdErrResults ${stderr}`)
-
-        core.setOutput("result", value);
-    } catch (error) {
-        if (error instanceof Error) {
-            core.setFailed(error.message); // Properly log the error message and fail the action
-        } else {
-            console.log('Failed to set');
-        }
-    }
-}
-
-async function main() {
-    const op = core.getInput('OPERATION');
-    switch (op) {
-        case "increment":
-            await increment();
-            break;
-        case "get":
-            await get();
-            break;
-        case "set":
-            await set();
-            break;
-        default:
-            core.setFailed("OPERATION must be one of [set | increment]")
-    }
-}
-
-increment();
-
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({ increment, get, set, main });
-
-})();
-
-module.exports = __webpack_exports__;
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __nccwpck_require__(2894);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
 /******/ })()
 ;
